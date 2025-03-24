05607a08665e5219b998d19b9687ec1830de7ea2
Francilene Silva <psilva.francilene@gmail.com>
2025-03-24 11:56:35 +0000

    test: tested de autenticação e metodos de depuração de código

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,20 @@
 const {defineConfig} = require('cypress')
 
 module.exports = defineConfig({
-    e2e: {
-        setupNodeEvents(on, config) {},
-        env: {
-            requestMode: true
-        }
+  e2e: {
+    baseUrl: 'https://restful-booker.herokuapp.com',
+    setupNodeEvents(on, config) {
     },
+    env: {
+      requestMode: true,
+      auth_url: '/auth',
+      body: {
+        username: "admin",
+        password: "password123"
+      }
+
+    }
+  },
 
 
-})
+});
--- cypress/e2e/auth.cy.js
+++ cypress/e2e/auth.cy.js
@@ -1,7 +1,40 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Test Suit - Auth API Testing', () => {
+
+  it('POST credentials to auth endpoint with success - 1ª version', () => {
+    cy.request({
+      method: 'POST',
+      url: '/auth',
+      body: {
+        "username": "admin",
+        "password": "password123"
+      },
+      headers: {'Content-Type': 'application/json'},
+      failOnStatusCode: false
+    }).then((response) => {
+      expect(response.status).to.eq(200);
+      expect(response.body).to.have.property('token').and.to.be.a('string');
+      expect(response.body).not.to.be.empty;
+    });
   });
+
+  it.only('POST credentials to auth endpoint with success - 2ª version', () => {
+
+    let body = {
+      "username": "admin",
+      "password": "password123"
+    };
+
+    cy.postRequest(Cypress.env('auth_url'), {"Content-type": "application/json"}, body)
+      .then((response) => {
+        //debugger;
+        expect(response.status).to.eq(200);
+        expect(response.body).to.have.property('token').and.to.be.a('string');
+        cy.pause()
+        expect(response.body).not.to.be.empty;
+      });
+  });
+
+
 
 
 });
